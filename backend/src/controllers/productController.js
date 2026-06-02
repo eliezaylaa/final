b4687c01e66ea8dd70b2693857a8453e35d45369
Elie <elie.zaylaa@epitech.eu>
2026-06-02 13:08:55 +0200

    feat: get product endpoint

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -8,5 +8,17 @@ const GetAllProducts = async (req, res) => {
     return res.json({ error: "Get products error" });
   }
 };
-
-module.exports = { GetAllProducts };
+const GetProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await pool.query("SELECT * FROM products WHERE id = $1", [
+      id,
+    ]);
+    if (product.rows.length == 0)
+      return res.json({ error: "Product not found" });
+    return res.json({ product: product.rows[0] });
+  } catch (error) {
+    return res.json({ error: "Get product error" });
+  }
+};
+module.exports = { GetAllProducts, GetProduct };
--- backend/src/routes/productRoute.js
+++ backend/src/routes/productRoute.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { GetAllProducts } = require("../controllers/productController");
+const {
+  GetAllProducts,
+  GetProduct,
+} = require("../controllers/productController");
 const { authenticate, admin } = require("../middleware/auth");
 
 router.get("/", authenticate, GetAllProducts);
+router.get(":id/", authenticate, GetProduct);
 
 module.exports = router;
