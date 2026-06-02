b100f43b905edf95b159db12e543c8ecf8cfc91e
Elie <elie.zaylaa@epitech.eu>
2026-06-02 10:18:14 +0200

    feat: Get shift endpoint

--- backend/src/controllers/shiftController.js
+++ backend/src/controllers/shiftController.js
@@ -10,5 +10,23 @@ const GetAllShifts = async (req, res) => {
     return res.json({ error: "Get all shifts error" });
   }
 };
+const GetShift = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-module.exports = { GetAllShifts };
+    const shift = await pool.query(
+      "SELECT s.id, u.full_name, u.role, s.date, s.start_time, s.end_time FROM shifts s JOIN users u ON u.id = s.user_id WHERE s.id = $1",
+      [id],
+    );
+
+    if (shift.rows.length == 0) {
+      return res.json({ error: "Shift not found" });
+    }
+
+    return res.json({ shift: shift.rows[0] });
+  } catch (error) {
+    return res.json({ error: "Get shift error" });
+  }
+};
+
+module.exports = { GetAllShifts, GetShift };
--- backend/src/routes/shiftRoute.js
+++ backend/src/routes/shiftRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { GetAllShifts } = require("../controllers/shiftController");
+const { GetAllShifts, GetShift } = require("../controllers/shiftController");
 
-const { authenticate, admin } = require("../middleware/auth");
+const { authenticate, admin, adminormanager } = require("../middleware/auth");
 
-router.get("/", authenticate, admin, GetAllShifts);
+router.get("/", authenticate, adminormanager, GetAllShifts);
+router.get("/:id", authenticate, adminormanager, GetShift);
 
 module.exports = router;
