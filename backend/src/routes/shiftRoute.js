ffc4890c662c2610bfdf2a126224995ec5a30774
Elie <elie.zaylaa@epitech.eu>
2026-06-02 10:50:41 +0200

    feat: addShift route  endpoint

--- backend/src/routes/shiftRoute.js
+++ backend/src/routes/shiftRoute.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { GetAllShifts, GetShift } = require("../controllers/shiftController");
+const {
+  GetAllShifts,
+  GetShift,
+  addShift,
+} = require("../controllers/shiftController");
 
 const { authenticate, admin, adminormanager } = require("../middleware/auth");
 
 router.get("/", authenticate, adminormanager, GetAllShifts);
 router.get("/:id", authenticate, adminormanager, GetShift);
-router.post("/", authenticate, admin);
+router.post("/", authenticate, admin, addShift);
 
 module.exports = router;
