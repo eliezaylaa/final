0a89eb3041f6f844f768e989144d73db940b140a
Elie <elie.zaylaa@epitech.eu>
2026-06-04 21:21:58 +0200

    fix: add check_in and check_out to getAllShifts

--- backend/src/controllers/shiftController.js
+++ backend/src/controllers/shiftController.js
@@ -3,7 +3,7 @@ const pool = require("../config/db");
 const GetAllShifts = async (req, res) => {
   try {
     const shifts = await pool.query(
-      "SELECT s.id, u.full_name, u.role, s.date, s.start_time, s.end_time FROM shifts s JOIN users u ON u.id = s.user_id ORDER BY s.date ASC",
+      "SELECT s.id, u.full_name, u.role, s.date, s.start_time, s.end_time, s.check_in, s.check_out  FROM shifts s JOIN users u ON u.id = s.user_id ORDER BY s.date ASC",
     );
     return res.status(200).json({ shifts: shifts.rows });
   } catch (error) {
